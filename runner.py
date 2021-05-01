59927c82ecafb8dd5b785459603e78563307a49f
roskey <lsjsg@outlook.com>
2021-05-01 19:56:39 +0800

    Added save loss, added exit, fix loss

--- runner.py
+++ runner.py
@@ -1,4 +1,6 @@
 import os.path
+
+import pandas as pd
 import torch
 from torch import nn
 from torch.autograd import Variable
@@ -17,38 +19,64 @@ def trainer(model, train_loader, testdataset, testsmb, critic, optimizer, epochs
     model = model.to(device)
     step = 0
     train_losses, test_losses = [], []
-    for epoch in range(1, epochs + 1):
-        total_train_loss = 0
-        count = 0
-        for feature, target in train_loader:
-            feature, target = Variable(feature.type(torch.FloatTensor)).to(device), Variable(
-                target.type(torch.FloatTensor)).to(device)
-            pred = model(feature)
-            loss = critic(pred.squeeze(1), target.float())
-            optim.zero_grad()
-            loss.backward(retain_graph=True)
-            nn.utils.clip_grad_norm_(model.parameters(), 0.1)
-            optim.step()
-            total_train_loss += loss.item()
-            step += 1
-            count += 1
-            if step % save_every == 0:
-                torch.save(model, os.path.join(save_path, "{}_model.h5".format(model.name)))
-            if step % eval_every == 0:
-                prediction_plot, predicted, actual = evaluate(model, testdataset, testsmb,
-                                                              split_at=test_split_at, device=device)
-                prediction_plot.savefig("plots/{}_{}_pred_and_actual.png".format(model.name, testdataset.glacier))
-                prediction_plot.close()
-                test_loss = critic(torch.tensor([predicted]), torch.tensor([actual])).item()
-
-                test_losses.append(test_loss)
-                mean_loss = total_train_loss / eval_every
-                train_losses.append(mean_loss)
-                print(
-                    "[INFO] Epoch {}|{} {} Loss: {:.4f} Eval: {:.4f}".format(epoch, epochs, step, mean_loss, test_loss))
-                loss_plot = plot_loss(train_losses, test_losses, show=show)
-                loss_plot.savefig("plots/{}_loss_plot.png".format(model.name))
-                loss_plot.close()
+    base_path = os.path.join(save_path, model.name)
+    if not os.path.exists(base_path):
+        os.makedirs(base_path)
+    if not os.path.exists(os.path.join(base_path, "plots")):
+        os.makedirs(os.path.join(base_path, "plots"))
+    try:
+        for epoch in range(1, epochs + 1):
+            total_train_loss = 0
+            count = 0
+            for feature, target in train_loader:
+                feature, target = Variable(feature.type(torch.FloatTensor)).to(device), Variable(
+                    target.type(torch.FloatTensor)).to(device)
+                pred = model(feature)
+                loss = critic(pred.squeeze(1), target.float())
+                optim.zero_grad()
+                loss.backward(retain_graph=True)
+                nn.utils.clip_grad_norm_(model.parameters(), 0.1)
+                optim.step()
+                total_train_loss += loss.item()
+                step += 1
+                count += 1
+                if step % save_every == 0:
+                    torch.save(model, os.path.join(base_path, "{}_model-{}.h5".format(model.name, epoch)))
+                if step % eval_every == 0:
+                    prediction_plot, predicted, actual = evaluate(model, testdataset, testsmb,
+                                                                  split_at=test_split_at, device=device)
+                    test_loss = critic(torch.tensor([predicted]), torch.tensor([actual])).item() / min(len(testdataset),
+                                                                                                       len(testsmb))
+                    test_losses.append(test_loss)
+                    mean_loss = total_train_loss / eval_every / loader.batch_size
+                    train_losses.append(mean_loss)
+                    prediction_plot.savefig(
+                        "{}/{}_{}_pred_and_actual-{}_{}_{}.png".format(os.path.join(base_path, "plots"), model.name,
+                                                                       testdataset.glacier, epoch,
+                                                                       loss.item() / loader.batch_size, test_loss))
+                    prediction_plot.close()
+                    print("[INFO] Epoch {}|{} {} Loss: {:.4f} Eval: {:.4f}".format(epoch, epochs, step, mean_loss,
+                                                                                   test_loss))
+                    loss_plot = plot_loss(train_losses, test_losses, show=show)
+                    loss_plot.savefig("{}/{}_loss_plot.png".format(os.path.join(base_path, "plots"), model.name))
+                    loss_plot.close()
+    except KeyboardInterrupt:
+        print("[INFO] Starting to exit!")
+    finally:
+        # save model
+        torch.save(model, os.path.join(base_path, "{}_model.h5".format(model.name)))
+        # loss plot
+        loss_plot = plot_loss(train_losses, test_losses, show=show)
+        loss_plot.savefig("{}/{}_loss_plot.png".format(os.path.join(base_path, "plots"), model.name))
+        loss_plot.close()
+        # loss record
+        pd.DataFrame({"train_loss": train_losses, "eval_loss": test_losses}).to_csv(os.path.join(base_path, "loss.csv"))
+        # Final evaluation
+        prediction_plot, predicted, actual = evaluate(model, testdataset, testsmb, split_at=test_split_at,
+                                                      device=device)
+        prediction_plot.savefig("{}/{}_{}_pred_and_actual.png".format(os.path.join(base_path, "plots"), model.name,
+                                                                      testdataset.glacier))
+        prediction_plot.close()
 
 
 def predict(model, dataset, device=None):
@@ -93,8 +121,10 @@ def evaluate(model, dataset, target, split_at=None, device=None):
 
 if __name__ == '__main__':
     # set dataset
-    JAKOBSHAVN_smb = Glacier_dmdt("JAKOBSHAVN_ISBRAE", 1980, 2002, path="glacier_dmdt.csv")
-    JAKOBSHAVN_data = ERA5Datasets("JAKOBSHAVN_ISBRAE", 1980, 2002, path="ECMWF_reanalysis_data")
+    JAKOBSHAVN_smb = Glacier_dmdt("JAKOBSHAVN_ISBRAE", 1980, 2010, path="glacier_dmdt.csv")
+    JAKOBSHAVN_data = ERA5Datasets("JAKOBSHAVN_ISBRAE", 1980, 2010, path="ECMWF_reanalysis_data")
+    JAKOBSHAVN_smb_test = Glacier_dmdt("JAKOBSHAVN_ISBRAE", 1980, 2018, path="glacier_dmdt.csv")
+    JAKOBSHAVN_data_test = ERA5Datasets("JAKOBSHAVN_ISBRAE", 1980, 2018, path="ECMWF_reanalysis_data")
     # QAJUUTTAP_SERMIA_smb = Glacier_dmdt("QAJUUTTAP_ISBRAE", 1980, 2002, path="glaicer_dmdt.csv")
     # QAJUUTTAP_SERMIA_data = ERA5Datasets("QAJUUTTAP_ISBRAE", 1980, 2002, path="ECMWF_reanalysis_data")
     # STORSTROMMEN_smb = Glacier_dmdt("STORSTROMMEN", 1980, 2002, path="glaicer_dmdt.csv")
@@ -122,6 +152,7 @@ if __name__ == '__main__':
     # train model
     cuda = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     loss_function = torch.nn.MSELoss()
-    trainer(glacier_model, train_loader=loader, testdataset=JAKOBSHAVN_data, testsmb=JAKOBSHAVN_smb, show=False,
-            device=cuda, epochs=3, lr=0.002, reg=0.001, save_every=10, eval_every=1, test_split_at=15,
+    trainer(glacier_model, train_loader=loader, testdataset=JAKOBSHAVN_data_test, testsmb=JAKOBSHAVN_smb_test,
+            show=False,
+            device=cuda, epochs=300, lr=0.002, reg=0.001, save_every=1, eval_every=1, test_split_at=2010,
             critic=loss_function, optimizer=torch.optim.Adam, save_path="saved_models")
