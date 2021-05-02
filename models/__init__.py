94a0a15ff5a5d18ecef09684ecc568975d384e25
roskey <lsjsg@outlook.com>
2021-05-02 17:20:08 +0800

    Update saving strategy

--- models/__init__.py
+++ models/__init__.py
@@ -50,14 +50,12 @@ class SeparateFeatureExtractor(nn.Module):
         self.output._apply(fn)
 
     def forward(self, x):
-        print(x.shape)
         features = self.flattener(x)
         result = []
         for model, data in zip(self.layers, features):
             result.append(model(data))
         out = torch.cat(result, dim=-1)
         out = self.output(out)
-        print(out.shape)
         return out
 
 
