7ea3010e8355af59584c75e12c97a6ddcb32ab6e
roskey <lsjsg@outlook.com>
2021-05-07 14:48:57 +0800

    Add last year smb as a feature to the predictor

--- models/predictors/LSTMPredictor.py
+++ models/predictors/LSTMPredictor.py
@@ -3,12 +3,15 @@ import torch.nn as nn
 
 
 class LSTMPredictor(nn.Module):
-    def __init__(self, input_dim=256, hidden_dim=[256, 128, 64, 32], p=0.5, **args):
+    def __init__(self, input_dim=256, hidden_dim=[256, 128, 64, 32], p=0.5, use_last_year_smb=False, **args):
         super(LSTMPredictor, self).__init__()
         assert "bidirection" in args
         assert "n_layers" in args
         if isinstance(hidden_dim, int):
             hidden_dim = [hidden_dim, hidden_dim]
+        if use_last_year_smb:
+            input_dim = input_dim + 1
+        self.use_last_year_smb = use_last_year_smb
         self.args = args
         self.lstm = nn.LSTM(input_size=input_dim, hidden_size=hidden_dim[0], batch_first=True,
                             bidirectional=self.args["bidirection"],
@@ -26,7 +29,11 @@ class LSTMPredictor(nn.Module):
         layer_reset.append(nn.Linear(hidden_dim[-1], 1))
         self.layer_final = nn.Sequential(*layer_reset)
 
-    def forward(self, x):
+    def forward(self, x, last_dmdt=None):
+        if self.use_last_year_smb:
+            assert last_dmdt is not None
+            assert last_dmdt.shape == (x.shape[0], 1)
+            x = torch.cat([x, last_dmdt], dim=1)
         x = torch.unsqueeze(x, 1)
         x, _ = self.lstm(x)
         x = self.activation(x)
